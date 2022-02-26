7a8acc44584df306a07ca16e808cffcc8718b978
RayhanHamada <rayhanhamada1999@gmail.com>
2022-02-27 00:29:01 +0700

    chore: clean

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,3 @@
 export default function Index() {
-  return (
-    <div className="w-full h-screen">
-      <div className="bg-blue-500 w-10 h-10 hover:animate-spin duration-1000"></div>
-    </div>
-  );
+  return <div>hello woRld</div>;
 }
