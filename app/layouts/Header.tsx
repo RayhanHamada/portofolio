f1a91224528dbe2334e9628f76f2836ddeabca69
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-05 20:03:49 +0700

    feat: improve header

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -25,15 +25,14 @@ const links = [
 
 const Header: React.FC = (_props) => {
   return (
-    <header className="flex flex-col w-full space-y-2 bg-[#202020] bg-opacity-90 border py-4 md:flex-row md:px-8 md:justify-between md:space-y-0">
-      <div>
-        <Link to="/">
-          <p className="text-center text-[#e41749] text-xl">
-            <span className="font-bold">Mocchapine |</span>{' '}
-            <span className="font-thin">Rayhan Hamada</span>
-          </p>
-        </Link>
-      </div>
+    <header className="flex flex-row w-full items-center space-y-2 bg-[#202020] bg-opacity-90 border py-4 px-4 md:flex-row md:justify-between md:space-y-0">
+      <Link to="/">
+        <p className="text-center text-[#e41749] text-xl">
+          <span className="text-[#e41749] text-xl align-bottom mr-2">&gt;</span>
+          <span className="font-bold">Mocchapine |</span>{' '}
+          <span className="font-thin">Rayhan Hamada</span>
+        </p>
+      </Link>
       <nav className="hidden md:block">
         <ul className="flex flex-col items-center align-middle justify-between space-y-1 text-white/75 md:flex-row md:space-x-4 md:items-center md:space-y-0">
           {links.map((link) => (
