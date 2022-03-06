a8fc26cea3c9bbe7151496b5125fa74b91cf2cb4
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-06 14:25:35 +0700

    feat: add border to sidenav

--- app/layouts/SideNav.tsx
+++ app/layouts/SideNav.tsx
@@ -25,7 +25,7 @@ const links = [
 
 const SideNav: React.FC = (_props) => {
   return (
-    <nav className="fixed z-9 px-4 h-full flex flex-col bg-[#202020] w-2 overflow-x-hidden hover:w-xs">
+    <nav className="fixed z-9 px-4 h-full flex flex-col bg-[#202020] w-2 overflow-x-hidden border-0 border-r-1 border-solid border-[#e41749] hover:w-xs">
       <ul className="relative py-4 flex flex-col space-y-4 top-[61px]">
         {links.map((link) => (
           <li key={link.to} className="my-0">
