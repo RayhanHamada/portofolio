8ad141f653df66d227ad565ce806f240b7a8da28
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-06 16:47:56 +0700

    feat: finish sidenav design

--- app/layouts/SideNav.tsx
+++ app/layouts/SideNav.tsx
@@ -1,36 +1,53 @@
+import { AiFillLinkedin, AiFillProject } from 'react-icons/ai';
+import { FaJs, FaPencilAlt, FaUser } from 'react-icons/fa';
+import { RiProfileFill } from 'react-icons/ri';
 import { Link } from 'remix';
 
 const links = [
   {
     to: '/',
     title: 'Me',
+    icon: FaUser,
   },
   {
     to: '/projects',
     title: 'Projects',
+    icon: AiFillProject,
   },
   {
     to: '/stack',
     title: 'Tech Stack',
+    icon: FaJs,
   },
   {
     to: '/contact',
     title: 'Contact',
+    icon: AiFillLinkedin,
   },
   {
     to: '/blogs',
     title: 'Blogs',
+    icon: FaPencilAlt,
+  },
+  {
+    to: '/resume',
+    title: 'My Resume',
+    icon: RiProfileFill,
   },
 ];
 
 const SideNav: React.FC = (_props) => {
   return (
-    <nav className="fixed z-9 px-4 h-full flex flex-col bg-[#202020] w-2 overflow-x-hidden border-0 border-r-1 border-solid border-[#e41749] hover:w-xs">
+    <nav className="fixed z-9 px-6 h-full flex flex-col bg-[#202020] w-10 overflow-x-hidden border-0 border-r-1 border-solid border-[#e41749] hover:w-xs">
       <ul className="relative py-4 flex flex-col space-y-4 top-[61px]">
-        {links.map((link) => (
-          <li key={link.to} className="my-0">
-            <Link className="hover:underline text-white" to={link.to}>
-              {link.title}
+        {links.map(({ title, to, icon: Icon }) => (
+          <li key={to} className="my-0">
+            <Link
+              className="hover:underline text-white flex flex-row items-center space-x-2"
+              to={to}
+            >
+              <span>{<Icon size="1.2em" style={{ color: 'white' }} />}</span>
+              <span className=""> {title}</span>
             </Link>
           </li>
         ))}
