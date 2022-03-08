6d51d6978f774bdd21bcbc105d4db6a205fa67a2
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-08 19:36:08 +0700

    feat: add footer

---
+++ app/layouts/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer: React.FC = (_props) => {
+  return (
+    <footer className="w-full flex flex-col items-center bg-my-red pt-20 pb-1">
+      <small className="text-[#e41749] text-center text-xs">
+        Website created by Muhammad Rayhan Hamada Budiman. Made using React and
+        Remix.
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
