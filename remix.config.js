d1940b49fb70e400cf1cf251592572c3c8e928f8
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-13 15:55:00 +0700

    chore: properly add vercel adapter

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,14 @@
+console.log(`currently in ${process.env.NODE_ENV} mode`);
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildDirectory: 'build',
-  devServerPort: 8002,
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['.*'],
+  // appDirectory: 'app',
+  // assetsBuildDirectory: 'public/build',
+  // publicPath: '/build/',
+  // serverBuildDirectory: 'build',
+  // devServerPort: 8002,
 };
