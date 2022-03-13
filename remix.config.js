5ad728889d0cdd42e3b02892deae8ddfd7170a55
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-13 16:41:08 +0700

    fix: reset

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,11 @@
-console.log(`currently in ${process.env.NODE_ENV} mode`);
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'build',
+  devServerPort: 8002,
   ignoredRouteFiles: ['.*'],
-  // appDirectory: 'app',
-  // assetsBuildDirectory: 'public/build',
-  // publicPath: '/build/',
-  // serverBuildDirectory: 'build',
-  // devServerPort: 8002,
 };
