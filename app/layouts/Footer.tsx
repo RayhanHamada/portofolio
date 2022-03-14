baaab4f5040c1a804ec44b394958a60e1017c86b
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-14 12:06:24 +0700

    feat: add links for footer

--- app/layouts/Footer.tsx
+++ app/layouts/Footer.tsx
@@ -1,10 +1,20 @@
 const Footer: React.FC = (_props) => {
   return (
-    <footer className="w-full flex flex-col items-center bg-my-red pt-10 pb-1 pl-15 md:px-15">
-      <small className="text-[#e41749] text-center text-xs">
-        Website created by Muhammad Rayhan Hamada Budiman. Made using React and
-        Remix.
-      </small>
+    <footer className="w-full flex flex-col items-center bg-my-red pt-20 pb-1 pl-15 md:px-15">
+      <p className="text-[#e41749] text-center text-xs">
+        <a href="/" className="underline">
+          This Website
+        </a>{' '}
+        created by Muhammad Rayhan Hamada Budiman. Made using{' '}
+        <a href="https://reactjs.org/" className="underline" target="_blank">
+          React
+        </a>{' '}
+        and{' '}
+        <a href="https://remix.run/" className="underline" target="_blank">
+          Remix
+        </a>
+        .
+      </p>
     </footer>
   );
 };
