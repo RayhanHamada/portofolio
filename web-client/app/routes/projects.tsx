4c5471b3913c623f0d5de25226dd96d27dc5070b
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-18 21:42:44 +0700

    feat: fallback to previous glob pattern and remove routeLayouts

--- web-client/app/routes/projects.tsx
+++ web-client/app/routes/projects.tsx
@@ -1,11 +1,28 @@
 import { ErrorBoundaryComponent, RouteComponent } from 'remix';
 import MainLayout from '~/layouts/MainLayout';
-import ProjectLayout from '~/layouts/routeLayouts/ProjectLayout';
 
 const Projects: RouteComponent = (_props) => {
   return (
     <MainLayout>
-      <ProjectLayout />
+      <div className="w-full flex flex-col">
+        <p className="text-center text-2xl text-[#e41749] font-bold">
+          My Projects
+        </p>
+        {/* list projects */}
+        <div className="w-full flex flex-col align-center">
+          <p className="text-center pt-8">
+            Under Construction, click{' '}
+            <a
+              href="https://github.com/RayhanHamada"
+              target="_blank"
+              className="underline"
+            >
+              Here
+            </a>{' '}
+            to see my projects
+          </p>
+        </div>
+      </div>
     </MainLayout>
   );
 };
