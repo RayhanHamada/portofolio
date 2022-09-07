3814d75a2c67789bb055afe0d3930961e3d3d168
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-09-07 23:32:53 +0700

    chore: add tailwindcss

--- web-client-svelte-kit/src/routes/+page.svelte
+++ web-client-svelte-kit/src/routes/+page.svelte
@@ -2,7 +2,7 @@
   console.log('Hello World');
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="font-mono">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
